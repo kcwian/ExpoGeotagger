6404c59769bdbc346ce7839a6228990edd3704d3
Krzysztof Ćwian <krzysztof.cwian@gmail.com>
2024-05-20 18:47:27 +0200

    Added instruction for .apk

--- screens/ModalCamera3Screen.tsx
+++ screens/ModalCamera3Screen.tsx
@@ -7,6 +7,16 @@ import { Text, View } from '../components/Themed';
 export default function ModalCamera3Screen() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Instrukcja:</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View>
+        <Text style={styles.text}>1. Udostępnić internet poprzez hotspot WiFi z telefonu (5Ghz).</Text>
+        <Text style={styles.text}>2. Włączyć urządzenie i połączyć je do udostępnionej sieci.</Text>
+        <Text style={styles.text}>3. Sprawdzić IP urządzenia (widoczne na górnym pasku po kliknięciu na ikonkę transmisji danych)</Text>
+        <Text style={styles.text}>4. Wpisać IP urządzenia w polu "Server IP" w ustawieniach tej aplikacji </Text>
+        <Text style={styles.text}>4. Uruchomić GeotagPhoneApp z pulpitu urządzenia </Text>
+        {/* Use a light status bar on iOS to account for the black space above the modal */}
+      </View>
       <Text style={styles.title}>Uwagi:</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/* <EditScreenInfo path="/screens/ModalScreen.tsx" /> */}
@@ -36,8 +46,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding:10
   },
+  text: {
+    fontSize: 16,
+    padding:10
+  },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
     width: '80%',
   },
